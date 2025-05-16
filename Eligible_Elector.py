cf6345fd3ed2170e1f4f993fc5556caed5aa4c30
ivan5355 <90850677+ivan5355@users.noreply.github.com>
2025-05-16 15:51:58 -0400

    Project1

---
+++ Eligible_Elector.py
@@ -0,0 +1,8 @@
+
+age = int(input("How old are you? "))
+
+# check if age is greater or equal to 18
+if age>=18:
+    print("Congratulations! You are eligible to vote. Go make a difference!")
+else:
+    print("Oops! You’re not eligible yet. But hey, only X more years to go!")
